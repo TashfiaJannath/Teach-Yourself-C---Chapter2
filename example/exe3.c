71ca79367bead11aade2294e0405e517cf02e1f5
Tashfia Jannath <76718979+TashfiaJannath@users.noreply.github.com>
2021-03-04 20:01:42 +0800

    Create exe3.c

---
+++ example/exe3.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(void)
+{
+    int num;
+    printf("Enter an integer:");
+    scanf("%d", &num);
+    if(num<0) printf("Number is negative.");
+    else printf("Number is non-negative");
+    return 0;
+    }
