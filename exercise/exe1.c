28c3cd90abfb1bddcb94260bb126916c37795787
Tashfia Jannath <76718979+TashfiaJannath@users.noreply.github.com>
2021-03-04 20:27:19 +0800

    Create exe1.c

---
+++ exercise/exe1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(void){
+    int i;
+    printf("Enter a number: ");
+    scanf("%d", &i);
+    if(i%2==0)
+    printf("even");
+    else {
+    printf("odd");}
+    return 0;
+    }
