f146dd7bfefef3e2770eaefa0c0d1adae8cb49d5
Tashfia Jannath <76718979+TashfiaJannath@users.noreply.github.com>
2021-03-04 20:01:14 +0800

    Create exa2.c

---
+++ example/exa2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(void)
+{
+    float num;
+    int choice;
+    printf("Enter value: ");
+    scanf("%f", &num);
+    printf("1.Feet to Meters, 2.Meters to Feet:");
+    printf("Enter choice: ");
+    scanf("%d", &choice);
+    if(choice==1)
+        printf("%f", num/3.28);
+    if(choice==2)
+    printf("%f", num*3.28);
+    return 0;
+    }
